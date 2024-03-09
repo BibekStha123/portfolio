514eccb238acdb2095a05a25c48a3f5274c83b08
Bibek <bibekstha084@gmail.com>
2024-03-09 17:34:09 +1030

    test

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from './components/Home'
 import About from './components/About'
+import Contact from "./components/Contact";
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     {
         path: '/about',
         element: <About/>
+    },
+    {
+        path: '/contact',
+        element: <Contact/>
     }
 ])
 
