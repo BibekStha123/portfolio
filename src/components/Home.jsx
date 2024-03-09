922c34b1e2903984d740cedcbbdda9a33ab945dc
Bibek <bibekstha084@gmail.com>
2024-03-09 17:30:12 +1030

    link added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home(props) {
     return (
         <div>
             <h2>Welcome to this site...</h2>
+            <Link to="/about">About</Link>
         </div>
     );
 }
